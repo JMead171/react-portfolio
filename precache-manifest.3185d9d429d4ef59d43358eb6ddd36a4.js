7c75c99960bc58f3777dcb9b802b02b0285e86f3
Jeff Mead <jeffdmead171@gmail.com>
2020-10-18 08:42:36 -0400

    Updates

---
+++ precache-manifest.3185d9d429d4ef59d43358eb6ddd36a4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcefc57c07fd548a645a3e47a01f2109",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "8bb57a23d6ed7f9b5f3e",
+    "url": "/react-portfolio/static/css/main.1df783b6.chunk.css"
+  },
+  {
+    "revision": "63a0549afb3854aafa76",
+    "url": "/react-portfolio/static/js/2.f801e135.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.f801e135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bb57a23d6ed7f9b5f3e",
+    "url": "/react-portfolio/static/js/main.8b4db298.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "0bede521be32b6ebbfc710e9f5979dd9",
+    "url": "/react-portfolio/static/media/1.0bede521.jpg"
+  },
+  {
+    "revision": "bee7f57f79ec841107f9ec6c8db4911d",
+    "url": "/react-portfolio/static/media/2.bee7f57f.jpg"
+  },
+  {
+    "revision": "4b484391113f2eabfe65ea71273a38ed",
+    "url": "/react-portfolio/static/media/3.4b484391.jpg"
+  },
+  {
+    "revision": "d8673264b9cc46a72678422457df71c7",
+    "url": "/react-portfolio/static/media/4.d8673264.jpg"
+  },
+  {
+    "revision": "6845eccdef720596ad14b1a3646bdea9",
+    "url": "/react-portfolio/static/media/5.6845eccd.jpg"
+  },
+  {
+    "revision": "f1f8e9e4dbdb0c5abe795c9c82574b6e",
+    "url": "/react-portfolio/static/media/9.f1f8e9e4.jpg"
+  },
+  {
+    "revision": "7d37ac4a4c5d3435ffb264067c346836",
+    "url": "/react-portfolio/static/media/jmresume.7d37ac4a.pdf"
+  }
+]);
